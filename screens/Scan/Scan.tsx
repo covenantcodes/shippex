29a74613f26774499d4fc07138952bfc63da1b82
Covenant Ifeoluwa <covenantcodes@Covenants-iMac-Pro.local>
2024-09-18 19:44:24 -0700

    Added base code for the tab screens

---
+++ screens/Scan/Scan.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { View, Text, StyleSheet, SafeAreaView } from "react-native";
+import COLORS from "../../configs/color";
+
+const Scan: React.FC = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.content}>
+        <Text style={styles.title}>Coming Soon!</Text>
+        <Text style={styles.details}>
+          We are actively working on this screen. You will get notified once it
+          is done.
+        </Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.primaryColor,
+  },
+  content: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: "bold",
+    color: COLORS.white,
+    marginBottom: 20,
+    fontFamily: "PoppinsSemiBold",
+  },
+  details: {
+    fontSize: 16,
+    color: COLORS.white,
+    textAlign: "center",
+    fontFamily: "PoppinsRegular",
+  },
+});
+
+export default Scan;
